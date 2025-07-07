3b345a6117060439f5e3c8df4a34ea0e789674ff
blinodev <linojanes@gmail.com>
2025-07-07 16:45:54 -0300

    update

--- app/model_rgline.py
+++
@@ -1,9 +0,0 @@
-from sklearn.linear_model import LinearRegression
-
-def train_model(X, y):
-    model = LinearRegression()
-    model.fit(X, y)
-    return model
-
-def predict(model, X_test):
-    return model.predict(X_test)
